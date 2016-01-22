c859975963ac9b5861fe1e5e36885894ef0a8738
kf4nvx <kf4nvx@gmail.com>
2016-01-22 12:10:35 -0500

    Update pinhash4.sh

--- pinhash4.sh
+++ pinhash4.sh
@@ -3,5 +3,5 @@ for pin in $(seq -f "%04g" 1 9999)
 do
  printf -v j "%04g" $pin
  hash=($(echo -n $pin |md5sum))
-echo "$j : $hash" >> /home/jonathan/scripts/pinhash/pinhash4.txt
+echo "$j : $hash" >> pinhash4.txt
 done
